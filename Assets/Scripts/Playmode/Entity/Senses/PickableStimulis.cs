2d00ccfaef4c5fc11d4f2d3c23386604f836550e
Simon <le6monstre@hotmail.com>
2018-08-30 13:01:34 -0400

    Ajout des channels

---
+++ Assets/Scripts/Playmode/Entity/Senses/PickableStimulis.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Playmode.Entity.Senses
+{
+
+    public class PickableStimulis : MonoBehaviour
+    {
+        private Pickable.PickableController controller;
+
+        private void Awake()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            controller = GetComponentInChildren<Pickable.PickableController>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            other.GetComponent<PickableSensor>()?.See(controller);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            other.GetComponent<PickableSensor>()?.LooseSightOf(controller);
+        }
+    }
+}
---
+++ Assets/Scripts/Playmode/Event/EventHandlerEnemyDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Playmode.Ennemy;
+namespace Playmode.Event
+{
+    public class EventHandlerEnemyDeath : EventChannel<EnemyDeathData>
+    {
+
+    }
+
+    public struct EnemyDeathData
+    {
+        public string name;
+
+        public EnemyDeathData(string name)
+        {
+            this.name = name;
+        }
+    }
+
+}
+
