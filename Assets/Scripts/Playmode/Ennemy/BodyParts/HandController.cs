257d5f6a2a495f9e646e53d2fdd08fef8fe6f4c6
FlyJonat <flyjonat@live.fr>
2018-08-31 10:24:01 -0400

    Ajout comportement cowboy tirer dans une autre direction

--- Assets/Scripts/Playmode/Ennemy/BodyParts/HandController.cs
+++ Assets/Scripts/Playmode/Ennemy/BodyParts/HandController.cs
@@ -46,5 +46,12 @@ namespace Playmode.Ennemy.BodyParts
                 weapon.Shoot();
             }
         }
+
+        public void SetRotationHand(Vector3 position)
+        {
+            Vector3 dir = position - transform.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        }
     }
 }
